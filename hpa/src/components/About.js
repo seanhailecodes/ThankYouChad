b0ff7306aef0a19c95e9080185cf8a9da0b54513
Sean Haile <seanhaile.codes@gmail.com>
2019-12-17 21:55:51 -0700

    updated a bit more Blue layout and formatting

--- hpa/src/components/About.js
+++ hpa/src/components/About.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Styles = styled.div`
+    .blueStyle {
+        background-color: #0000FF;
+        background-size: cover;
+        color: #0000ff;
+    }
+    `
 
 export const About = () => (
-    <div>
+
+  <div className='blueStyle'>
     <h2>About Us</h2>
     <p> 
           We have a team of of proven experts, at your disposal. Drawing on decades of experience across twenty-four multi-disciplinary specialties, we will provide your practice with hands on support - improving your bottom line. 
       </p>
   </div>
+ 
 )
--- hpa/src/components/Home.js
+++ hpa/src/components/Home.js
@@ -1,23 +1,6 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
-import styled from 'styled-components'
 
-const Styles = styled.div`
-    .overlay {
-        background-color: #0000FF;
-        opacitiy: 0.6;
-        position: absolute;
-        top: 0;
-        left: 0;
-        bottom; 0:
-        right: 0;
-        z-index: -1;
-    }
-
-`
 export const Home = () => (
-    <Styles>
-       <Container>
             <div>
             <h2>When is the last time your practice had a check up?</h2>
             <br></br>
@@ -26,6 +9,4 @@ export const Home = () => (
                 Revenue
             </p>
             </div>
-        </Container>
-    </Styles>
 )
